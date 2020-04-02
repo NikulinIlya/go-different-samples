ecf51293613f8adee2a97e1f75d35f2b10e72d10
NikulinIlya <ilia.nikulin@gmail.com>
2020-04-02 12:30:03 +0300

    feat(rest-api/main.go): add Article, func allArticles

--- rest-api/main.go
+++ rest-api/main.go
@@ -1,10 +1,30 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 )
+
+// Article :
+type Article struct {
+	Title   string `json:"Title"`
+	Desc    string `json"desc"`
+	Content string `json:"content"`
+}
+
+// Articles :
+type Articles []Article
+
+func allArticles(w http.ResponseWriter, r *http.Request) {
+	articles := Articles{
+		Article{Title: "Test Title", Desc: "Test Description", Content: "Hello world"},
+	}
+
+	fmt.Println("Endpoint Hit: All Articles Endpoint")
+	json.NewEncoder(w).Encode(articles)
+}
 
 func homePage(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Homepage Endpoint Hit")
@@ -12,6 +32,7 @@ func homePage(w http.ResponseWriter, r *http.Request) {
 
 func handleRequest() {
 	http.HandleFunc("/", homePage)
+	http.HandleFunc("/articles", allArticles)
 	log.Fatal(http.ListenAndServe(":8081", nil))
 }
 
