44fed58ed6b1935daf9b890f8d0902c3bbb128aa
NikulinIlya <ilia.nikulin@gmail.com>
2020-05-08 18:27:28 +0300

    feat(go-orm/main.go): add routes to handleRequests

--- go-orm/main.go
+++ go-orm/main.go
@@ -15,13 +15,17 @@ func helloWorld(w http.ResponseWriter, r *http.Request) {
 func handleRequests() {
 	myRouter := mux.NewRouter().StrictSlash(true)
 	myRouter.HandleFunc("/", helloWorld).Methods("GET")
-	myRouter.HandleFunc("/users", AllUsers).Methods("ET")
-	yRouter.HandleFunc()
-log.Fatal(http.ListenAndServe(":8081", myRouter))
+	myRouter.HandleFunc("/users", AllUsers).Methods("GET")
+	myRouter.HandleFunc("/user/{name}/{email}", NewUser).Methods("POST")
+	myRouter.HandleFunc("/user/{name}", DeleteUser).Methods("DELETE")
+	myRouter.HandleFunc("/user/{name}/{email}", UpdateUser).Methods("PUT")
+	log.Fatal(http.ListenAndServe(":8081", myRouter))
 }
 
-unc main() {
-	fmt.Println("Go RM sample")
+func main() {
+	fmt.Println("Go ORM sample")
 
+	InitialMigration()
+	
 	handleRequests()
 }
